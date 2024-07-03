dc50b1dee8f1c647d6f47c988d66657ef7bb4153
Luiz Henrique Rodrigues Da Silva <rodriguesluizhenrique316@gmail.com>
2024-07-03 08:29:41 -0300

    Aula de enum

---
+++ src/main/java/org/example/Entities/Enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package org.example.Entities.Enums;
+
+public enum OrderStatus {
+
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPPED,
+    DELIVILERED;
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,23 @@
 package org.example;
 
+import org.example.Entities.Enums.OrderStatus;
+import org.example.Entities.Order;
+
+import java.util.Date;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Aula de Git e Github!");
+        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(order);
+
+        OrderStatus os1 = OrderStatus.DELIVILERED;
+
+        OrderStatus os2 = OrderStatus.valueOf("DELIVILERED");
+
+        System.out.println(os1);
+        System.out.println(os2);
     }
 }
