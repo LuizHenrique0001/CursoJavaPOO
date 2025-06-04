76acfc7ed6898ee684234f77dd527d6461f9fbbf
Luizhenrique0001 <rodriguesluizhenrique316@gmail.com>
2025-06-04 14:53:03 -0300

    Exercicio de "Juros e Imposto"

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,51 @@
-import Model.Entity.Account;
-import Model.Exeptions.AccontExeption;
-import org.w3c.dom.DOMException;
+import Model.Entity.Contract;
+import Model.Entity.Installment;
+import Model.Service.ContractService;
+import Model.Service.PaypalService;
 
 import java.text.ParseException;
-import java.util.*;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
 
-        try{
-            Locale.setDefault(Locale.US);
-            Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-            System.out.println("Enter account data: ");
-            System.out.print("Number: ");
-            int number = sc.nextInt();
-            sc.nextLine();
-            System.out.print("Holder: ");
-            String holder = sc.nextLine();
-            System.out.print("Initial balance: ");
-            double initialBalace = sc.nextDouble();
-            System.out.print("Withdraw limit: ");
-            double withdrawLimit = sc.nextDouble();
+        System.out.println("Entre os dados do contrato: ");
+        System.out.print("Numero: ");
+        int number = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Data (dd/MM/yyyy): ");
 
-            Account account = new Account(initialBalace, holder, number, withdrawLimit);
+        String stringDate = sc.nextLine();
 
-            System.out.print("Enter amount for withdraw:");
-            double amount = sc.nextDouble();
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate date = LocalDate.parse(stringDate, fmt);
 
-            account.withdraw(amount);
+        System.out.print("Valor do contrato: ");
+        double valueContract = sc.nextDouble();
 
-            System.out.printf("New balance: %.2f", account.getBalance());
+        Contract contract = new Contract(date, number, valueContract);
 
-            sc.close();
+        System.out.print("Entre com o numero de parcelas: ");
+        int numberInstallments = sc.nextInt();
+
+        ContractService contractService = new ContractService(new PaypalService());
+        contractService.processContract(contract, numberInstallments);
+
+        System.out.println("Parcelas: ");
+        for (Installment item: contract.getInstallments()){
+            System.out.println(item);
         }
-        catch (AccontExeption e) {
-            System.out.print(e.getMessage());
-        }
-        catch (RuntimeException e){
-            System.out.print(e.getMessage());
-        }
+
+
+        sc.close();
+
     }
 }
---
+++ src/Model/Interface/OnlinePaymentService.java
@@ -0,0 +1,7 @@
+package Model.Interface;
+
+public interface OnlinePaymentService {
+
+    public Double paymentFee(Double amount);
+    public Double interest(Double amount, Integer months);
+}
